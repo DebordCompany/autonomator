44bde3a674e754f9e91fcd2aed06ab0987e8aac5
DebordCompany <debord.info@gmail.com>
2023-06-08 01:11:47 +0200

    corregido error sobre texto principal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
     <title>Autonomator</title>
 </svelte:head>
 <h1>Bienvenidos a autonomator</h1>
-<h2>El portal para el autionomo que no es digital</h2>
+<h2>El portal para el autonomo que no es digital</h2>
 
